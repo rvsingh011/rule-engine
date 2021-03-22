4fabac8df2b015b4abc88f67705b0fee382e204a
rvsingh011@gmail.com <rvsingh011@gmail.com>
2021-03-22 20:37:50 +0530

    Test working

--- benchmark/execute_benchmark_test.go
+++ benchmark/execute_benchmark_test.go
@@ -29,60 +29,128 @@ import (
   Please refer docs/benchmarking_en.md for more info
 */
 
-var knowledgeBase *ast.KnowledgeBase
+var FactKB *ast.KnowledgeBase
+var AuthKB *ast.KnowledgeBase
 
-func Benchmark_Grule_Execution_Engine(b *testing.B) {
-	rules := []struct {
-		name string
-		fun  func(string)
-		filename string
-	}{
-		{"100 rules", load100RulesIntoKnowledgebase, "rules-1.grl"},
-		{"100 rules", load100RulesIntoKnowledgebase, "rules-2.grl"},
-		{"100 rules", load100RulesIntoKnowledgebase, "rules-3.grl"},
-		{"100 rules", load100RulesIntoKnowledgebase, "rules-4.grl"},
+var FactFile = "./fact-file.grl"
+var authFactFile = "./auth-file.grl"
 
-	}
-	for _, rule := range rules {
-		for k := 0; k < 10; k++ {
-			b.Run(fmt.Sprintf("%s", rule.name), func(b *testing.B) {
-				rule.fun(rule.filename)
-				for i := 0; i < b.N; i++ {
-					f1 := RideFact{
-						Distance: 6000,
-						Duration: 121,
-					}
-					e := engine.NewGruleEngine()
-					//Fact1
-					dataCtx := ast.NewDataContext()
-					err := dataCtx.Add("Fact", &f1)
-					if err != nil {
-						b.Fail()
-					}
-					err = e.Execute(dataCtx, knowledgeBase)
-					if err != nil {
-						fmt.Print(err)
-					}
-				}
-			})
-		}
-	}
+
+type Facts struct {
+    IsReversal       bool
+    AlreadyReversed  bool
+    IsCurrentBatch bool
+    IsPreviousBatch bool
+    Action        string
 }
 
-func load100RulesIntoKnowledgebase(filename string) {
+func (mf *Facts) GetAction(typeString string) string {
+    return fmt.Sprintf("%s", typeString)
+}
+
+var reversalFact = []Facts{
+	{
+		IsReversal: true,
+		AlreadyReversed: false,
+		IsPreviousBatch: true,
+	},{
+		IsReversal: false,
+		AlreadyReversed: false,
+		IsPreviousBatch: true,
+	},
+}
+
+type AuthFacts struct {
+    IsAuth       bool
+    Target       string
+    IsNetworking bool
+    IsRequestMsg bool
+    UnknownEvent bool
+    Types        string
+    Name         string
+    Handlers     []string
+}
+
+func (mf *AuthFacts) GetType(typeString string) string {
+    return fmt.Sprintf("%s", typeString)
+}
+func (mf *AuthFacts) GetName(nameString string) string {
+    return fmt.Sprintf("%s", nameString)
+}
+func (mf *AuthFacts) GetHandler(aString []string, subString string, subString2 string) []string {
+    val := fmt.Sprintf("%s", subString)
+    val2 := fmt.Sprintf("%s", subString2)
+    aString = append(aString, val, val2)
+    return aString
+}
+
+var authFacts = []AuthFacts{
+	{
+		IsAuth : true ,
+		Target: "Hello",
+		IsNetworking: true,
+	},
+	{
+		IsAuth : true ,
+		Target: "Hfnwe",
+		IsNetworking: true,
+	},
+}
+
+
+
+func TestGrule_Execution_Engine(b *testing.T) {
+	// build Knowledge base 
+	FactKB = CreateKnowledgeBase(FactFile, "fact")
+	AuthKB = CreateKnowledgeBase(authFactFile, "authFact")
+
+
+	e:= engine.NewGruleEngine()
+	dataCtx := ast.NewDataContext()
+	err := dataCtx.Add("facts", &reversalFact[0])
+	if err != nil {
+		fmt.Println("There was a error", err.Error())
+	}
+	err = e.Execute(dataCtx, FactKB)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+
+	fmt.Println(reversalFact[0])
+
+	// // check for multiple facts 
+	// for k := 0; k < 10; k++ {
+	// 	b.Run(fmt.Sprintf("%s", rule.name), func(b *testing.B) {
+	// 		rule.fun(rule.filename)
+	// 		for i := 0; i < b.N; i++ {
+				
+	// 			e := engine.NewGruleEngine()
+	// 			//Fact1
+	// 			dataCtx := ast.NewDataContext()
+	// 			err := dataCtx.Add("Fact", &f1)
+	// 			if err != nil {
+	// 				b.Fail()
+	// 			}
+	// 			err = e.Execute(dataCtx, knowledgeBase)
+	// 			if err != nil {
+	// 				fmt.Print(err)
+	// 			}
+	// 		}
+	// 	})
+	// }
+
+
+}
+
+func CreateKnowledgeBase(filename string, FactType string) *ast.KnowledgeBase{
 	input, _ := ioutil.ReadFile(filename)
 	rules := string(input)
-	fact := &RideFact{
-		Distance: 6000,
-		Duration: 121,
-	}
-	dctx := ast.NewDataContext()
-	_ = dctx.Add("Fact", fact)
-
 	lib := ast.NewKnowledgeLibrary()
 	rb := builder.NewRuleBuilder(lib)
-	_ = rb.BuildRuleFromResource("exec_rules_test", "0.1.1", pkg.NewBytesResource([]byte(rules)))
-	knowledgeBase = lib.NewKnowledgeBaseInstance("exec_rules_test", "0.1.1")
+
+	_ = rb.BuildRuleFromResource(FactType, "0.1.1", pkg.NewBytesResource([]byte(rules)))
+	return  lib.NewKnowledgeBaseInstance(FactType, "0.1.1")
 }
 
 
